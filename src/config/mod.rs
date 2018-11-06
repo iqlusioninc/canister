cf66659ae3b8d0811c47ba49eb6c19c2f899b5b9
Shella Stephens <shella@iqlusion.io>
2018-11-06 14:08:21 -0800

    updated canister.toml.example & RunCommandConfig struct

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -10,6 +10,13 @@ pub struct CanisterConfig {
     pub tag: String,
     pub object: String,
     pub path: PathBuf,
+    pub run_command: RunCommandConfig,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct RunCommandConfig {
+    pub path: PathBuf,
+    pub args: Vec<String>
 }
 
 impl_global_config!(CanisterConfig, GLOBAL_CONFIG);
